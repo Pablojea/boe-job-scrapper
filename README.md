794890e4c3c5b3ba42794497fff0273f17612dde
Pablojea <pablo_ojea@outlook.com>
2023-08-27 12:06:31 +0200

    updates readme and cleans some code

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # boe-job-scrapper
 Parses Spanish BOE to collect public administration job vacancies based on preference parameters
+
+single_day_mode: If single_day_mode is set to True, will collect data from yesterday BOE.
+If single_day_mode is set to False, will parse the number of days provided in timespan variable
+
+time_span : Is the number of days to collect data from
+
+area : Is the name of the province to search offers in. Must be provided in the format '(province_name')
--- boe-scrapper.py
+++ boe-scrapper.py
@@ -14,12 +14,12 @@ def scrap_day(day) -> None:
         if area + ', referente a la convocatoria para proveer' in offer.text:
             offer_text = offer.find_element(by=By.TAG_NAME, value="p").text
             offer_link = offer.find_element(by=By.TAG_NAME, value="a").get_attribute(name="href")
-            offerList.append((offer_text, offer_link))
+            offer_list.append((offer_text, offer_link))
 
 
 # parameters
-singleDayMode = False # True: scrappes yesterday offers / False: scrappes 'timespan' of days backwards from yesterday
-timespan = 15
+single_day_mode = False # True: scrappes yesterday offers / False: scrappes 'timespan' of days backwards from yesterday
+timespan = 5
 area = '(Pontevedra)'
 
 # browser configuration
@@ -32,9 +32,9 @@ driver.implicitly_wait(2) # Increase wait time for slower internet conections
 # scrapping
 searchDate = (datetime.date.today() - datetime.timedelta(days=1)).strftime('%Y/%m/%d')
 url_template = 'https://www.boe.es/boe/dias/' + searchDate + '/index.php?s=2B'
-offerList = []
+offer_list = []
 
-if singleDayMode:
+if single_day_mode:
     scrap_day(searchDate)
 else:
     for i in range(1, timespan + 1):
@@ -43,5 +43,5 @@ else:
         if weekday != 6:
             scrap_day(searchDate)
 
-for row in offerList:
-    print(row[0] + '  -> ' + row[1])
+for row in offer_list:
+    print(row[0] + '\n  -> ' + row[1])
